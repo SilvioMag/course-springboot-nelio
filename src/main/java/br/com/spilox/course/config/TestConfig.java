1a67319d0038d0bbd931d1d6f0b60844e3ca7ad2
SilvioMag <silviomagalhaes62@gmail.com>
2021-07-08 22:14:17 -0300

    Payment, one-to-one association

--- src/main/java/br/com/spilox/course/config/TestConfig.java
+++ src/main/java/br/com/spilox/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.spilox.course.entities.Category;
 import br.com.spilox.course.entities.Order;
 import br.com.spilox.course.entities.OrderItem;
+import br.com.spilox.course.entities.Payment;
 import br.com.spilox.course.entities.Product;
 import br.com.spilox.course.entities.User;
 import br.com.spilox.course.entities.enums.OrderStatus;
@@ -84,7 +85,9 @@ public class TestConfig implements CommandLineRunner {
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
-	
 }
--- src/main/java/br/com/spilox/course/entities/Order.java
+++ src/main/java/br/com/spilox/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -22,9 +24,8 @@ import br.com.spilox.course.entities.enums.OrderStatus;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -40,6 +41,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -77,7 +81,6 @@ public class Order implements Serializable {
 		}
 	}
 
-
 	public User getClient() {
 		return client;
 	}
@@ -88,6 +91,14 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	
 	@Override
