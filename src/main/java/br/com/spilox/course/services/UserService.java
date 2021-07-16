dbbc26ef703e4a1daaa620cbcb6ef5536bdfe7b1
SilvioMag <silviomagalhaes62@gmail.com>
2021-07-16 14:31:36 -0300

    Exception handling - delete

--- src/main/java/br/com/spilox/course/services/UserService.java
+++ src/main/java/br/com/spilox/course/services/UserService.java
@@ -4,39 +4,50 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.spilox.course.entities.User;
 import br.com.spilox.course.repositories.UserRepository;
+import br.com.spilox.course.services.exceptions.DatabaseException;
 import br.com.spilox.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Integer id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User user) {
 		return userRepository.save(user);
 	}
-	
+
 	public void delete(Integer id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Integer id, User user) {
-		User entity = userRepository.getById(id); 
+		User entity = userRepository.getById(id);
 		updateData(entity, user);
-	
+
 		return userRepository.save(entity);
 	}
 
